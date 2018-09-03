8b0f4cdc002b5425c4a4fcb1c444f454795b60a2
prashant.kumar <prashant.kumar@milvik.se>
2018-09-03 21:31:57 +0530

    PostCreateView and PostUpdateView added and working

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -3,6 +3,7 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
+    PostUpdateView,
 )
 from . import views
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('create/', PostCreateView.as_view(), name='post-create'),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about'),
 
 ]
